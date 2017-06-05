d1424b0b321c1425bf3b8dac3abc01f4532cd3ff
shoji-asakura <52kamikita41@gmail.com>
2017-06-05 17:59:26 +0900

    lesson25

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,11 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::resource('todo', 'TodoController');
+//通信方法がなんであろうが渡す
+//resource(どこのURL（ページ）が欲しいか,どこのメソッドを呼び出すか)
+//todoフォルダの中のURLすべてにアクセスされたら
 Route::get('/', function () {
     return view('welcome');
+    //view - URLを表示させる。
 });
