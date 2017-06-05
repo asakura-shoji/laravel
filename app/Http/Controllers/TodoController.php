300085cfe63084a0bd20406ba20f64bd01ffa850
shoji-asakura <52kamikita41@gmail.com>
2017-06-05 19:40:31 +0900

    lesson26

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,6 +10,7 @@ use App\Http\Requests;
 
 class TodoController extends Controller {
   private $todo;
+  //private - クラス内からでしかアクセスできない
 
   public function __construct(Todo $todo){
     //__construct - クラスにおいてオブジェクトが生成された際に実行されるメソッド
@@ -29,5 +30,24 @@ class TodoController extends Controller {
     //viewヘルパーに渡している最初の引数はディレクトリー中のビューファイル名
     //２つ目の引数は、ビューで使用するデータの配列
     //compact - 変数を受け渡す
+  }
+
+  public function create() {
+    return view('todo.create');
+    //view - URLを表示
+  }
+
+  public function store(Request $request) {
+    $input = $request->all();
+    //$request->all() - 入力されたフォームの内容を取得
+    $this->todo->fill($input);
+    //fill()を使うとEloquentは$fillableをチェックして、このプロパティを設定できるかどうかを確認する。
+    $this->todo->save();
+    //save() - 新しいレコードがデータベースに挿入されます。
+    //created_atとupdated_atタイムスタンプは自動的に設定されます。
+    //saveメソッドはデータベースで既に存在するモデルを更新するためにも使用されます。
+
+    return redirect()->to('todo');
+    //保存後にToDo一覧にリダイレクトするようにしている
   }
 }
--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+  protected $fillable = ['title'];
+  //protected -
+  //$fillable - ホワイトリスト_複数代入時に代入を許可する属性を配列で設定
+  //insertやcreateメソッドでテーブルのカラムに値を複数代入する際に、
+  //予期せぬ代入(製作者が意図していない代入)が起こることを防ぐ(id等)
+
+  //Formヘルパーを使って送信するとCSRF対策として自動でランダム文字列も送ってくれます
+  //ただこの_tokenの値は保存しないので必要な項目のみ登録するようにしている
 }
